16a808e9785b616fdb7c43ce6968e8261c53eaa7
Tobias Johansson <tobias.johansson@seal-software.com>
2017-10-26 12:18:33 +0200

    http poc

--- src/main/java/com/seal/vertx/App.java
+++ src/main/java/com/seal/vertx/App.java
@@ -1,86 +1,37 @@
 package com.seal.vertx;
 
-import com.seal.vertx.verticles.ConsumerVerticle;
-import com.seal.vertx.verticles.ProducerVerticle;
-import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
-import io.vertx.core.json.JsonObject;
-import io.vertx.core.spi.cluster.ClusterManager;
-import io.vertx.servicediscovery.Record;
-import io.vertx.servicediscovery.ServiceDiscovery;
-import io.vertx.servicediscovery.types.MessageSource;
-import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Random;
-
-/**
- * Created by jacobsznajdman on 20/09/17.
- */
 public class App {
     private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        String type = args[0];
+        Vertx vertx = Vertx.vertx();
 
-        ClusterManager mgr = new HazelcastClusterManager();
-        VertxOptions options = new VertxOptions().setClusterManager(mgr)
-                .setClustered(true)
-                .setClusterHost("127.0.0.1")
-                .setHAEnabled(true)
-                .setHAGroup("dev")
-                .setWorkerPoolSize(1);
-        Vertx.clusteredVertx(options, res -> {
-            if (res.succeeded()) {
-                Vertx vertx = res.result();
-                if (type.equals("producer")) {
-                    deployProducer(vertx);
-                } else if (type.equals("consumer")) {
-                    deployConsumer(vertx);
-                } else {
-                    throw new RuntimeException("Usage: java App.class [producer|consumer]");
-                }
-            } else {
-                // failed!
-            }
+        HttpServer server = vertx.createHttpServer();
+
+        Router router = Router.router(vertx);
+        router.route("/client/*").handler(StaticHandler.create("client"));
+        router.route().handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("content-type", "text/plain");
+            response.end("Hello World from Vert.x-Web!");
         });
 
+//        SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+//        BridgeOptions bridgeOptions = new BridgeOptions();
+//        PermittedOptions permittedOptions = new PermittedOptions();
+//        permittedOptions.
+//        bridgeOptions.addInboundPermitted()
+//        sockJSHandler.bridge()
+        server.requestHandler(router::accept).listen(8080);
+
     }
 
-    private static void deployConsumer(Vertx vertx) {
-        Random rnd = new Random();
-        int intAddress = rnd.nextInt();
-        String address = String.valueOf(intAddress);
-        ServiceDiscovery discovery = ServiceDiscovery.create(vertx);
-        DeploymentOptions opts = new DeploymentOptions().setWorker(true);
-        deployConsumer(vertx, address, discovery, opts);
-    }
-
-    private static void deployConsumer(Vertx vertx, String address, ServiceDiscovery discovery, DeploymentOptions opts) {
-        vertx.deployVerticle(new ConsumerVerticle(address), opts, ar -> {
-            LOG.info("Consumer deployed at address {}, id {}.", address, ar.result());
-            Record record = new Record()
-                    .setType(MessageSource.TYPE)
-                    .setLocation(new JsonObject().put("address", address))
-                    .setName("consumer");
-            discovery.publish(record, pr -> {
-                if (pr.succeeded()) {
-                    // publication succeeded
-                    Record publishedRecord = pr.result();
-                    LOG.info("Published service {}", publishedRecord.getName());
-                } else {
-                    // publication failed
-                }
-            });
-        });
-    }
-
-    private static void deployProducer(Vertx vertx) {
-        DeploymentOptions opts = new DeploymentOptions().setWorker(false);
-        vertx.deployVerticle(new ProducerVerticle(), opts, ar -> {
-            LOG.info("Producer deployed with id {}", ar.result());
-        });
-    }
 }
