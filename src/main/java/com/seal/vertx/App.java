750b82d4191b80ae8e24b6fa2c4bf596ed6ce6cd
Tobias Johansson <tobias.johansson@seal-software.com>
2017-10-26 13:05:52 +0200

    sockjs poc

--- src/main/java/com/seal/vertx/App.java
+++ src/main/java/com/seal/vertx/App.java
@@ -5,6 +5,9 @@ import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,18 +21,28 @@ public class App {
 
         Router router = Router.router(vertx);
         router.route("/client/*").handler(StaticHandler.create("client"));
+
+        // pipe ws <-> vertx bux
+        SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+        BridgeOptions bridgeOptions = new BridgeOptions();
+        PermittedOptions permitAll = new PermittedOptions();
+        permitAll.setAddressRegex(".*");
+        bridgeOptions.addInboundPermitted(permitAll);
+        bridgeOptions.addOutboundPermitted(permitAll);
+        SockJSHandler bridge = sockJSHandler.bridge(bridgeOptions);
+
+        router.route("/eventbus/*").handler(bridge);
+
         router.route().handler(routingContext -> {
             HttpServerResponse response = routingContext.response();
             response.putHeader("content-type", "text/plain");
-            response.end("Hello World from Vert.x-Web!");
+            response.end("catchall");
         });
 
-//        SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
-//        BridgeOptions bridgeOptions = new BridgeOptions();
-//        PermittedOptions permittedOptions = new PermittedOptions();
-//        permittedOptions.
-//        bridgeOptions.addInboundPermitted()
-//        sockJSHandler.bridge()
+        vertx.eventBus().consumer("draw", msg -> {
+           LOG.info("Got: {}", msg.body());
+        });
+
         server.requestHandler(router::accept).listen(8080);
 
     }
